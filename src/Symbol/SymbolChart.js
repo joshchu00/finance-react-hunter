673eba353eadac0adf8e37604bf366cfe45d38b4
Josh Chu <>
2019-01-20 19:24:44 +0800

    Add volume and refactor

--- src/Symbol/SymbolChart.js
+++ src/Symbol/SymbolChart.js
@@ -14,14 +14,44 @@ class SymbolChart extends Component {
 
     const { records } = this.props;
 
+    const DATE = 'date';
+    const LOW = 'low';
+    const OPEN = 'open';
+    const CLOSE = 'close';
+    const HIGH = 'high';
+    const VOLUME = 'volume';
+    const VOLUMESTYLE = 'volumeStyle';
+    const SOMETHING = 'something';
+
+    const getDataIndex = (name) => {
+
+      let index = -1;
+    
+      if (name === DATE) { index = 0; }
+      else if (name === LOW) { index = 1; }
+      else if (name === OPEN) { index = 2; }
+      else if (name === CLOSE) { index = 3; }
+      else if (name === HIGH) { index = 4; }
+      else if (name === VOLUME) { index = 5; }
+      else if (name === VOLUMESTYLE) { index = 6; }
+      else if (name === SOMETHING) { index = 7; }
+    
+      return index;
+    };
+
+    const getDataIndexes = (names) => names.map(x => getDataIndex(x))
+    
+    const volumeStyle = (open, close) => open < close ? '#0f9d58' : '#a52714';
+
     const data = google.visualization.arrayToDataTable([[
-        {type: 'date', label: 'date'},
-        {type: 'number', label: 'low'},
-        {type: 'number', label: 'open'},
-        {type: 'number', label: 'close'},
-        {type: 'number', label: 'high'},
-        {type: 'number', label: 'volume'},
-        {type: 'number', label: 'something'},
+        {type: 'date', label: DATE},
+        {type: 'number', label: LOW},
+        {type: 'number', label: OPEN},
+        {type: 'number', label: CLOSE},
+        {type: 'number', label: HIGH},
+        {type: 'number', label: VOLUME},
+        {role: 'style'},
+        {type: 'number', label: SOMETHING},
         // {type: 'string', role: 'annotation'},
         // {type: 'string', role: 'annotationText'},
       ],]
@@ -33,8 +63,9 @@ class SymbolChart extends Component {
             record.open,
             record.close,
             record.high,
+            record.volume,
+            volumeStyle(record.open, record.close),
             22 + Math.random() * 3,
-            20 + Math.random() * 10,
           ]
         )
       )
@@ -76,19 +107,40 @@ class SymbolChart extends Component {
             'color': 'black', 
           }, 
           '1': {
-            'type': 'bars',
-            'color': 'black', 
+            'type': 'line',
+            'color': 'blue',
           },
         },
       },
-      'view': {'columns': [0, 1, 2, 3, 4, 5]}
+      'view': {
+        'columns': getDataIndexes([DATE, LOW, OPEN, CLOSE, HIGH, SOMETHING]),
+      },
+    });
+
+    var volume = new google.visualization.ChartWrapper({
+      'chartType': 'ColumnChart',
+      'containerId': 'symbol-volume',
+      'options': {
+        'width': '100%',
+        'height': '150',
+        'legend': 'none',
+        'chartArea': {
+          'width': '80%',
+        },
+        'bar': {
+          'groupWidth': '80%',
+        },
+      },
+      'view': {
+        'columns': getDataIndexes([DATE, VOLUME, VOLUMESTYLE]),
+      },
     });
 
     var control = new google.visualization.ControlWrapper({
       'controlType': 'ChartRangeFilter',
       'containerId': 'symbol-control',
       'options': {
-        'filterColumnIndex': 0,
+        'filterColumnIndex': getDataIndex('date'),
         'ui': {
           'chartType': 'LineChart',
           'chartOptions': {
@@ -100,7 +152,7 @@ class SymbolChart extends Component {
             'hAxis': { 'baselineColor': 'none' },
           },
           'chartView': {
-            'columns': [0, 3],
+            'columns': getDataIndexes([DATE, CLOSE]),
           },
         },  
       },
@@ -120,10 +172,13 @@ class SymbolChart extends Component {
           'groupWidth': '80%',
         },
       },
-      'view': {'columns': [0, 6]}
+      'view': {
+        'columns': getDataIndexes([DATE, SOMETHING]),
+      },
     });
 
     dashboard.bind(control, candlestick);
+    dashboard.bind(control, volume);
     dashboard.bind(control, column);
 
     dashboard.draw(data);
@@ -133,6 +188,7 @@ class SymbolChart extends Component {
     return (
       <div id='symbol-chart' className='Symbol-chart'>
         <div id='symbol-candlestick'></div>
+        <div id='symbol-volume'></div>
         <div id='symbol-control'></div>
         <div id='symbol-column'></div>
       </div>
