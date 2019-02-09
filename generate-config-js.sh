f36b66ee9987c073877df35ed119db4d691b533a
Josh Chu <>
2019-02-09 18:34:25 +0800

    Modify script

--- generate-config-js.sh
+++ generate-config-js.sh
@@ -1,35 +1,5 @@
 #!/bin/sh
 
-# if [ -z ${ENVIRONMENT} ]; then
-#     ENVIRONMENT=undefined
-# else
-#     ENVIRONMENT=${ENVIRONMENT}
-# fi
-
-# if [ -z ${HUNTER_PORT} ]; then
-#     HUNTER_PORT=undefined
-# else
-#     HUNTER_PORT=${HUNTER_PORT}
-# fi
-
-# if [ -z ${SHIELDER_PROXY_SCHEME} ]; then
-#     SHIELDER_PROXY_SCHEME=undefined
-# else
-#     SHIELDER_PROXY_SCHEME=${SHIELDER_PROXY_SCHEME}
-# fi
-
-# if [ -z ${SHIELDER_PROXY_HOST} ]; then
-#     SHIELDER_PROXY_HOST=undefined
-# else
-#     SHIELDER_PROXY_HOST=${SHIELDER_PROXY_HOST}
-# fi
-
-# if [ -z ${SHIELDER_PROXY_PORT} ]; then
-#     SHIELDER_PROXY_PORT=undefined
-# else
-#     SHIELDER_PROXY_PORT=${SHIELDER_PROXY_PORT}
-# fi
-
 cat <<EOF
 window.config = {}
 window.config.environment='${ENVIRONMENT}';
